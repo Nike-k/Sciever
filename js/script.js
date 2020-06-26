6ffaccae67c08a353e2330920661e63ae51cd68a
Nikesh <niku.mhr.111@gmail.com>
2020-06-26 16:00:48 +0545

    First Commit

---
+++ js/script.js
@@ -0,0 +1,64 @@
+
+
+/* =========================================
+               Responsive Tabs
+============================================ */
+$(function () {
+
+    $("#services-tabs").responsiveTabs({
+        animation: 'slide'
+    });
+
+});
+
+$(function () {
+
+    $("#services-tab").responsiveTabs({
+        animation: 'slide'
+    });
+
+});
+
+
+
+
+/* =========================================
+               Magnifier
+============================================ */
+$(function () {
+
+    $("#portfolio-wrapper").magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+
+});
+
+
+
+
+     
+
+
+
+/* =========================================
+                Animation
+============================================ */
+// animate on scroll
+$(function () {
+    new WOW().init();
+});
+
+// home animation on page load
+$(window).on('load', function () {
+
+    $("#home-heading-1").addClass("animated fadeInDown");
+    $("#home-heading-2").addClass("animated fadeInLeft");
+    $("#home-text").addClass("animated zoomIn");
+    $("#home-btn").addClass("animated zoomIn");
+    $("#arrow-down i").addClass("animated fadeInDown infinite");
+
+});
